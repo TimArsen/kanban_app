162b006fca716c4c6749cd9383622b6c6e6657f5
Tim Arsenault <timarsen@gmail.com>
2016-05-16 07:33:09 -0600

    setup react and hotloading

--- webpack.config.js
+++ webpack.config.js
@@ -8,16 +8,44 @@ const PATHS = {
 	build: path.join(__dirname, 'build')
 };
 
+process.env.BABEL_ENV = TARGET;
+
 const common = {
 	entry: {
 		app: PATHS.app
 	},
+	// Add resolve.extensions.
+  	// '' is needed to allow imports without an extension.
+  	// Note the .'s before extensions as it will fail to match without!!!
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
 	output: {
 		path: PATHS.build,
 		filename: 'bundle.js'
-	}
-};
+	},
+	module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      },
+      // Set up jsx. This accepts js too thanks to RegExp
+      {
+        test: /\.jsx?$/,
+        // Enable caching for improved performance during development
+        // It uses default OS directory by default. If you need something
+        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+        loaders: ['babel?cacheDirectory'],
+        // Parse only app files! Without this it will go through entire project.
+        // In addition to being slow, that will most likely result in an error.
+        include: PATHS.app
+      }
 
+    ]
+  }
+};
 
 // Default configuration. We will return this if
 // Webpack is called outside of npm.
